7dc42a2781dbbac0c3ae5d5ea52ac6dd44314bf3
Rodrigo Sendin <rodrigo@algorama.com.br>
2021-02-06 11:06:41 -0300

    Hello World

---
+++ helloworld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Pós 2021!");
+        }
+    }
+}
