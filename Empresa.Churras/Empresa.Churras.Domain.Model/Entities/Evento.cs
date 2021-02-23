d005eef3ff3b9667358270f9c82c1ca09622f62b
Rodrigo Sendin <rodrigo@algorama.com.br>
2021-02-23 15:07:38 -0300

    Objetos de Valor

--- Empresa.Churras/Empresa.Churras.Domain.Model/Entities/Evento.cs
+++ Empresa.Churras/Empresa.Churras.Domain.Model/Entities/Evento.cs
@@ -1,15 +1,25 @@
-﻿using Kernel.Domain.Model.Entities;
+﻿using Empresa.Churras.Domain.Model.ValueObjects;
+using Kernel.Domain.Model.Entities;
 using System;
+using System.Collections.Generic;
 
 namespace Empresa.Churras.Domain.Model.Entities
 {
     public class Evento : EntityKeySeq
     {
-        public string DonoDaCasa { get; set; }
+        public Colega DonoDaCasa { get; set; }
+        public long DonoDaCasaKey { get; set; }
+        public string Nome { get; set; }
         public string Tipo { get; set; }
         public DateTime Dia { get; set; }
-        public string Periodo { get; set; }
-        public string ColegasConfirmados { get; set; }
-        public string OqueCadaUmVaiLevar { get; set; }
+        public Periodo Periodo { get; set; }
+        public List<EventoColegaConfirmado> ColegasConfirmados { get; set; }
+    }
+
+    public class EventoColegaConfirmado
+    {
+        public long ColegaId { get; set; }
+        public string ColegaNome { get; set; }
+        public string VaiLevar { get; set; }
     }
 }
---
+++ Empresa.Churras/Empresa.Churras.Domain.Model/ValueObjects/Endereco.cs
@@ -0,0 +1,11 @@
+﻿using Kernel.Domain.Model.ValueObjects;
+
+namespace Empresa.Churras.Domain.Model.ValueObjects
+{
+    public class Endereco : ValueObject<Endereco>
+    {
+        public string Descricao { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+    }
+}
