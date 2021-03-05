11204d3d1edb2b5bdf4fddc5d37d2405bb171449
Rodrigo Sendin <rodrigo@algorama.com.br>
2021-03-05 10:37:56 -0300

    Ajustes IoC e MockUserProvider

---
+++ Kernel/Kernel.Infra/Mock/MockUserProvider.cs
@@ -0,0 +1,31 @@
+﻿using Kernel.Domain.Model.Dtos;
+using Kernel.Domain.Model.Providers;
+using System;
+using System.Threading.Tasks;
+
+namespace Kernel.Infra.Mock
+{
+    public class MockUserProvider : IUserProvider
+    {
+        public async Task<Token> GetToken()
+        {
+            var token = new Token
+            {
+                Email = "user@tes.com.br",
+                Name = "Test User"
+            };
+
+            return await Task.FromResult(token);
+        }
+
+        public Task RemoveToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveTokenString(string tokenString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
