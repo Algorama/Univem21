2b8fca98a3d8ba45560b7d7cfdc958b39f8381b1
Rodrigo Sendin <rodrigo@algorama.com.br>
2021-08-14 09:18:34 -0300

    Implementação do Teste de Insert do Evento

--- Univem.Churras/Univem.Churras.Tests/RepositoryTest.cs
+++ Univem.Churras/Univem.Churras.Tests/RepositoryTest.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Univem.Churras.Domain.Model.Entities;
+using Univem.Churras.Domain.Model.Enums;
 using Univem.Churras.Domain.Model.ValueObjects;
 using Univem.Churras.Infra;
 
@@ -38,7 +39,7 @@ namespace Univem.Churras.Tests
         [TestMethod]
         public async Task Insert_Colega_Test()
         {
-            using(var session = new SessionRepository(_context))
+            using (var session = new SessionRepository(_context))
             {
                 var repo = session.GetRepository<Colega>();
 
@@ -46,8 +47,8 @@ namespace Univem.Churras.Tests
                 {
                     Key = 1,
                     Nome = "Tião Carrero",
-                    Endereco = new Endereco 
-                    { 
+                    Endereco = new Endereco
+                    {
                         Cidade = "Piracicaba",
                         UF = "SP",
                         Descricao = "Casa do Tião"
@@ -55,6 +56,64 @@ namespace Univem.Churras.Tests
                 };
 
                 await repo.Insert(tiao);
+
+                session.SaveChanges();
+            }
+        }
+
+        [TestMethod]
+        public async Task Insert_Evento_Test()
+        {
+            using (var session = new SessionRepository(_context))
+            {
+                var repoColega = session.GetRepository<Colega>();
+                var repoEvento = session.GetRepository<Evento>();
+
+                var tiao = new Colega
+                {
+                    Key = 1001,
+                    Nome = "Tião Carrero",
+                    Endereco = new Endereco
+                    {
+                        Cidade = "Piracicaba",
+                        UF = "SP",
+                        Descricao = "Casa do Tião"
+                    }
+                };
+
+                var pardinho = new Colega
+                {
+                    Key = 1002,
+                    Nome = "Pardinho",
+                    Endereco = new Endereco
+                    {
+                        Cidade = "Limeira",
+                        UF = "SP",
+                        Descricao = "Sítio do Pardinho"
+                    }
+                };
+
+                await repoColega.Insert(tiao);
+                await repoColega.Insert(pardinho);
+
+                var evento = new Evento
+                {
+                    Key = 1001,
+                    DonoDaCasa = tiao,
+                    Tipo = TipoEvento.Churras,
+                    Nome = "Churras na casa do Tião",
+                    Dia = new DateTime(2021, 8, 21),
+                    Periodo = new Periodo
+                    {
+                        Inicio = new DateTime(2021, 8, 21, 12, 0, 0),
+                        Fim = new DateTime(2021, 8, 21, 12, 0, 0)
+                    }
+                };
+
+                evento.ConfirmarPresenca(tiao);
+                evento.ConfirmarPresenca(pardinho);
+
+                await repoEvento.Insert(evento);
 
                 session.SaveChanges();
             }
